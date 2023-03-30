51937a3e096dde0d19eb7427d61dd4c2b99e8a29
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-03-30 20:10:01 -0300

    Update test case name and minimal id accepted

--- src/test_requests.py
+++ src/test_requests.py
@@ -1,9 +1,8 @@
 from Request import *
 import pytest
 
-def test_auto_id():
+def test_auto_increment_id():
     class_counter = Request.class_counter
-
     first_request = Request(1, "water bottle")
     second_request = Request(2, "water bottle")
     first_id = first_request.get_id()
@@ -12,10 +11,10 @@ def test_auto_id():
     assert second_id is class_counter + 1
 
 
-def test_get_id():
+def test_id_valtype():
     check_id_req = Request(1, "water bottle")
     number = check_id_req.get_id()
-    assert number >= 0 and isinstance(number, int)
+    assert number >= 1 and isinstance(number, int)
 
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
