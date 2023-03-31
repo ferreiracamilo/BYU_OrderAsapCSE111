9db7a0776d0eedd828d274331d19d713d65ae9ac
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-03-30 21:58:32 -0300

    Add total and subtotal functions
    Add subtotal, total functions along defining imports, adding blank lines

--- src/Order.py
+++ src/Order.py
@@ -1,10 +1,16 @@
 from datetime import datetime
+from Request import *
+from Product import *
+from Customer import *
+
 
 class Order:
     CATEGORIES = ["Waiting", "In Process", "Delivered"]
 
+
     #INDEXES start at ZERO but this will work as order id as well, therefore enforcing start at 1
     class_counter = 1
+
 
     def __init__(self, customer, discount=0, tax_rate=12):
         #Not every order will be eligible or receive a discount, therefore is enforced ZERO
@@ -18,6 +24,7 @@ class Order:
         self._tax_rate = tax_rate
         Order.class_counter += 1
 
+
     def get_id(self):
         """Retrieve id
 
@@ -25,6 +32,7 @@ class Order:
             Int: id
         """
         return self._id
+
 
     def get_customer(self):
         """Retrieve customer
@@ -34,6 +42,7 @@ class Order:
         """
         return self._customer
 
+
     def get_date(self):
         """Retrieve date
 
@@ -41,6 +50,7 @@ class Order:
             Date: date
         """
         return self._date
+
 
     def get_discount(self):
         """Retrieve discount
@@ -50,6 +60,7 @@ class Order:
         """
         return self._discount
 
+
     def get_requests(self):
         """Retrieve requests
 
@@ -57,6 +68,7 @@ class Order:
             List{Request}: requests
         """
         return self._requests
+
 
     def get_tax_rate(self):
         """Retrieve tax_rate
@@ -66,6 +78,7 @@ class Order:
         """
         return self._tax_rate
 
+
     def get_status(self):
         """Retrieve status
 
@@ -73,6 +86,7 @@ class Order:
             String: status
         """
         return self._status
+
 
     def set_status(self, status):
         """Update status
@@ -82,6 +96,7 @@ class Order:
         """
         self._status = status
 
+
     def set_discount(self, discount):
         """Update discount
 
@@ -89,6 +104,7 @@ class Order:
             discount (Float): discount
         """
         self._status = discount
+
 
     def set_tax_rate(self, tax_rate):
         """Update tax_rate
@@ -98,10 +114,47 @@ class Order:
         """
         self._status = tax_rate
 
+
     def set_requests(self, requests_list):
+        """Update _requests
+
+        Args:
+            requests_list (List[requests]): requests
+        """
+        self._requests = requests_list
+    
+    def add_request(self, request):
         """Update tax_rate
 
         Args:
-            tax_rate (List[requests]): requests
+            request (Request): request
         """
-        self._requests = requests_list
+        self._requests.append(request)
+
+
+    def calculate_subtotal(self):
+        """Iterate the request list to calculate the summatory of product prices
+
+        Returns:
+            float: subtotal before taxes and discounts
+        """
+        subtotal = 0
+        if len(self._requests) > 0:
+            for request in self._requests:
+                req_prod = request.get_product()
+                req_qty = request.get_quantity()
+                subtotal += req_prod.get_price() * req_qty
+        return subtotal
+
+
+    def calculate_total(self):
+        """Iterate the request list to calculate the summatory of product prices
+
+        Returns:
+            float: total after taxes and discounts
+        """
+        total = self.calculate_subtotal()
+        if total > 0:
+            total = total * (1+self._discount/100)
+            total = total * (1+self._tax_rate/100)
+        return total
