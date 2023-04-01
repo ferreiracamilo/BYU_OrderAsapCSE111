d16d70d959a46182750b3d47ab07282ff7b5f8a4
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-04-01 19:25:13 -0300

    remove unneeded comment

--- src/Utils.py
+++ src/Utils.py
@@ -1,7 +1,6 @@
 import re, os, shutil
 
 def validate_email(myvalue):
-        #Local function
         pat = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
         if re.match(pat,myvalue):
             return True
