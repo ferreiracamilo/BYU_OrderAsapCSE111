5df49db073ffff7c88a9c1072436b9ed4fec08e8
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-04-02 01:48:14 -0300

    Gui 1st version

---
+++ src/gui-main.py
@@ -0,0 +1,297 @@
+import tkinter as tk
+from tkinter import *
+import tkinter.font as tkFont
+
+class App:
+    def __init__(self, root):
+        #setting title
+        root.title("Invoice Generator")
+        #setting window size
+        width=600
+        height=500
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.geometry(alignstr)
+        root.resizable(width=False, height=False)
+
+        lbl_cx_name=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_cx_name["font"] = ft
+        lbl_cx_name["fg"] = "#333333"
+        lbl_cx_name["justify"] = "center"
+        lbl_cx_name["text"] = "Customer Name:"
+        lbl_cx_name.place(x=28,y=40,width=100,height=25)
+
+        txt_cx_name=tk.Entry(root)
+        txt_cx_name["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_cx_name["font"] = ft
+        txt_cx_name["fg"] = "#333333"
+        txt_cx_name["justify"] = "center"
+        txt_cx_name["text"] = "name input"
+        txt_cx_name.place(x=129,y=40,width=186,height=30)
+        txt_cx_name["show"] = "name input"
+
+        lbl_cx_lastname=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_cx_lastname["font"] = ft
+        lbl_cx_lastname["fg"] = "#333333"
+        lbl_cx_lastname["justify"] = "center"
+        lbl_cx_lastname["text"] = "Customer Last Name:"
+        lbl_cx_lastname.place(x=3,y=80,width=125,height=25)
+
+        txt_cx_lastname=tk.Entry(root)
+        txt_cx_lastname["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_cx_lastname["font"] = ft
+        txt_cx_lastname["fg"] = "#333333"
+        txt_cx_lastname["justify"] = "center"
+        txt_cx_lastname["text"] = "lastname input"
+        txt_cx_lastname.place(x=129,y=80,width=186,height=30)
+
+        lbl_cx_billing_address=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_cx_billing_address["font"] = ft
+        lbl_cx_billing_address["fg"] = "#333333"
+        lbl_cx_billing_address["justify"] = "center"
+        lbl_cx_billing_address["text"] = "Billing Address:"
+        lbl_cx_billing_address.place(x=38,y=120,width=90,height=25)
+
+        txt_cx_billing_address=tk.Entry(root)
+        txt_cx_billing_address["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_cx_billing_address["font"] = ft
+        txt_cx_billing_address["fg"] = "#333333"
+        txt_cx_billing_address["justify"] = "center"
+        txt_cx_billing_address["text"] = "234 Ron St, Opa Locka, Billing"
+        txt_cx_billing_address.place(x=129,y=120,width=186,height=30)
+
+        lbl_cx_phone=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_cx_phone["font"] = ft
+        lbl_cx_phone["fg"] = "#333333"
+        lbl_cx_phone["justify"] = "center"
+        lbl_cx_phone["text"] = "Phone number:"
+        lbl_cx_phone.place(x=318,y=80,width=92,height=25)
+
+        txt_cx_phone=tk.Entry(root)
+        txt_cx_phone["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_cx_phone["font"] = ft
+        txt_cx_phone["fg"] = "#333333"
+        txt_cx_phone["justify"] = "center"
+        txt_cx_phone["text"] = "phone input"
+        txt_cx_phone.place(x=410,y=80,width=186,height=30)
+
+        lbl_cx_email=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_cx_email["font"] = ft
+        lbl_cx_email["fg"] = "#333333"
+        lbl_cx_email["justify"] = "center"
+        lbl_cx_email["text"] = "Email"
+        lbl_cx_email.place(x=368,y=120,width=40,height=25)
+
+        txt_cx_email=tk.Entry(root)
+        txt_cx_email["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_cx_email["font"] = ft
+        txt_cx_email["fg"] = "#333333"
+        txt_cx_email["justify"] = "center"
+        txt_cx_email["text"] = "email input"
+        txt_cx_email.place(x=410,y=120,width=186,height=30)
+
+        lbl_cx_birthdate=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_cx_birthdate["font"] = ft
+        lbl_cx_birthdate["fg"] = "#333333"
+        lbl_cx_birthdate["justify"] = "center"
+        lbl_cx_birthdate["text"] = "Birthdate:"
+        lbl_cx_birthdate.place(x=348,y=40,width=60,height=25)
+
+        txt_cx_email=tk.Entry(root)
+        txt_cx_email["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_cx_email["font"] = ft
+        txt_cx_email["fg"] = "#333333"
+        txt_cx_email["justify"] = "center"
+        txt_cx_email["text"] = "MM/DD/YYYY"
+        txt_cx_email.place(x=410,y=40,width=186,height=30)
+
+        lbl_cx_shipping_address=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_cx_shipping_address["font"] = ft
+        lbl_cx_shipping_address["fg"] = "#333333"
+        lbl_cx_shipping_address["justify"] = "center"
+        lbl_cx_shipping_address["text"] = "Shipping Address:"
+        lbl_cx_shipping_address.place(x=20,y=160,width=105,height=25)
+
+        txt_cx_shipping_address=tk.Entry(root)
+        txt_cx_shipping_address["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_cx_shipping_address["font"] = ft
+        txt_cx_shipping_address["fg"] = "#333333"
+        txt_cx_shipping_address["justify"] = "center"
+        txt_cx_shipping_address["text"] = "234 Ron St, Opa Locka, Shipping"
+        txt_cx_shipping_address.insert(0,"St Address, City, State, Zip Code")
+        txt_cx_shipping_address.place(x=129,y=160,width=186,height=30)
+
+        lbl_cx_section=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_cx_section["font"] = ft
+        lbl_cx_section["fg"] = "#333333"
+        lbl_cx_section["justify"] = "center"
+        lbl_cx_section["text"] = "*** CUSTOMER INFORMATION ***"
+        lbl_cx_section.place(x=200,y=5,width=200,height=25)
+
+        lbl_prods_section=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_prods_section["font"] = ft
+        lbl_prods_section["fg"] = "#333333"
+        lbl_prods_section["justify"] = "center"
+        lbl_prods_section["text"] = "*** PRODUCT LIST ***"
+        lbl_prods_section.place(x=230,y=210,width=140,height=25)
+
+        lbl_prods_water=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_prods_water["font"] = ft
+        lbl_prods_water["fg"] = "#333333"
+        lbl_prods_water["justify"] = "center"
+        lbl_prods_water["text"] = "Sprinkle Water 1lt - 3.99 USD"
+        lbl_prods_water.place(x=10,y=260,width=171,height=30)
+
+        lbl_prods_flour=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_prods_flour["font"] = ft
+        lbl_prods_flour["fg"] = "#333333"
+        lbl_prods_flour["justify"] = "center"
+        lbl_prods_flour["text"] = "Flour 1kg - 1.40 USD"
+        lbl_prods_flour.place(x=60,y=290,width=127,height=30)
+
+        lbl_prods_sugar=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_prods_sugar["font"] = ft
+        lbl_prods_sugar["fg"] = "#333333"
+        lbl_prods_sugar["justify"] = "center"
+        lbl_prods_sugar["text"] = "Sugar 1kg - 0.40 USD"
+        lbl_prods_sugar.place(x=60,y=320,width=121,height=30)
+
+        lbl_prods_towel=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_prods_towel["font"] = ft
+        lbl_prods_towel["fg"] = "#333333"
+        lbl_prods_towel["justify"] = "center"
+        lbl_prods_towel["text"] = "Paper Towel 500pcs - 2.20 USD"
+        lbl_prods_towel.place(x=335,y=260,width=173,height=36)
+
+        lbl_prods_toilet=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_prods_toilet["font"] = ft
+        lbl_prods_toilet["fg"] = "#333333"
+        lbl_prods_toilet["justify"] = "center"
+        lbl_prods_toilet["text"] = "Toilet Paper 300pcs - 5.80 USD"
+        lbl_prods_toilet.place(x=334,y=290,width=174,height=30)
+
+        lbl_prods_garbage=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_prods_garbage["font"] = ft
+        lbl_prods_garbage["fg"] = "#333333"
+        lbl_prods_garbage["justify"] = "center"
+        lbl_prods_garbage["text"] = "Garbage Can 1lt - 3.20 USD"
+        lbl_prods_garbage.place(x=330,y=320,width=163,height=30)
+
+        txt_prods_water=tk.Entry(root)
+        txt_prods_water["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_prods_water["font"] = ft
+        txt_prods_water["fg"] = "#333333"
+        txt_prods_water["justify"] = "center"
+        txt_prods_water["text"] = "water1"
+        txt_prods_water.place(x=190,y=260,width=70,height=25)
+
+        txt_prods_flour=tk.Entry(root)
+        txt_prods_flour["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_prods_flour["font"] = ft
+        txt_prods_flour["fg"] = "#333333"
+        txt_prods_flour["justify"] = "center"
+        txt_prods_flour["text"] = "flour1"
+        txt_prods_flour.place(x=190,y=290,width=70,height=25)
+
+        txt_prods_sugar=tk.Entry(root)
+        txt_prods_sugar["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_prods_sugar["font"] = ft
+        txt_prods_sugar["fg"] = "#333333"
+        txt_prods_sugar["justify"] = "center"
+        txt_prods_sugar["text"] = "sugar1"
+        txt_prods_sugar.place(x=190,y=320,width=70,height=25)
+
+        txt_prods_towel=tk.Entry(root)
+        txt_prods_towel["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_prods_towel["font"] = ft
+        txt_prods_towel["fg"] = "#333333"
+        txt_prods_towel["justify"] = "center"
+        txt_prods_towel["text"] = "towel1"
+        txt_prods_towel.place(x=520,y=260,width=70,height=25)
+
+        txt_prods_toilet=tk.Entry(root)
+        txt_prods_toilet["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_prods_toilet["font"] = ft
+        txt_prods_toilet["fg"] = "#333333"
+        txt_prods_toilet["justify"] = "center"
+        txt_prods_toilet["text"] = "toiler1"
+        txt_prods_toilet.place(x=520,y=290,width=70,height=25)
+
+        txt_prods_garbage=tk.Entry(root)
+        txt_prods_garbage["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times',size=10)
+        txt_prods_garbage["font"] = ft
+        txt_prods_garbage["fg"] = "#333333"
+        txt_prods_garbage["justify"] = "center"
+        txt_prods_garbage["text"] = "garb1"
+        txt_prods_garbage.place(x=520,y=320,width=70,height=25)
+
+        lbl_accumulated_section=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_accumulated_section["font"] = ft
+        lbl_accumulated_section["fg"] = "#333333"
+        lbl_accumulated_section["justify"] = "center"
+        lbl_accumulated_section["text"] = "*** ACCUMULATED ***"
+        lbl_accumulated_section.place(x=220,y=360,width=152,height=30)
+
+        lbl_accumulated_total=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_accumulated_total["font"] = ft
+        lbl_accumulated_total["fg"] = "#333333"
+        lbl_accumulated_total["justify"] = "center"
+        lbl_accumulated_total["text"] = "Total: 308 USD"
+        lbl_accumulated_total.place(x=310,y=390,width=166,height=31)
+
+        lbl_accumulated_subtotal=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=10)
+        lbl_accumulated_subtotal["font"] = ft
+        lbl_accumulated_subtotal["fg"] = "#333333"
+        lbl_accumulated_subtotal["justify"] = "center"
+        lbl_accumulated_subtotal["text"] = "Subtotal: 290 USD"
+        lbl_accumulated_subtotal.place(x=110,y=390,width=166,height=31)
+
+        btn_generate_invoice=tk.Button(root)
+        btn_generate_invoice["bg"] = "#f0f0f0"
+        ft = tkFont.Font(family='Times',size=10)
+        btn_generate_invoice["font"] = ft
+        btn_generate_invoice["fg"] = "#000000"
+        btn_generate_invoice["justify"] = "center"
+        btn_generate_invoice["text"] = "Generate Invoice"
+        btn_generate_invoice.place(x=250,y=450,width=100,height=25)
+        btn_generate_invoice["command"] = self.btn_generate_invoice_comman
+
+    def btn_generate_invoice_comman(self):
+        print("command")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
